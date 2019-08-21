c00178d4c00eff9629f1067fbd0b7a629da077dd
Vikram Jakhar <vikram.jakhar@tothenew.com>
2019-08-21 22:55:19 +0530

    Guess the number

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::{Rng, Error};
+
+fn main() {
+    println!("Guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+
+        println!("The secret number is {}", secret_number);
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+        }
+    }
+}
